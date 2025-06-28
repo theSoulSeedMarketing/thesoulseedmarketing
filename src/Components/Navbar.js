49a9d2c17665c87421371c12db64eab9e3c75925
Atharva Jaurkar <atharva@strategyzinc.com>
2025-06-28 17:04:01 +0530

    made navbar responsive, website is now responsive for smaller size windows

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,27 +20,11 @@ import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
-    {
-      text: "Home",
-      icon: <HomeIcon />,
-    },
-    {
-      text: "About",
-      icon: <InfoIcon />,
-    },
-    {
-      text: "Testimonials",
-      icon: <CommentRoundedIcon />,
-    },
-    {
-      text: "Contact",
-      icon: <PhoneRoundedIcon />,
-    },
-    {
-      text: "Cart",
-      icon: <ShoppingCartRoundedIcon />,
-    },
-  ];
+  { text: "Home", icon: <HomeIcon />, href: "#home" },
+  { text: "About", icon: <InfoIcon />, href: "#about" },
+  { text: "Testimonials", icon: <CommentRoundedIcon />, href: "#testimonials" },
+  { text: "Contact", icon: <PhoneRoundedIcon />, href: "#contact" },
+];
   return (
     <nav>
       <div className="nav-logo-container">
@@ -67,16 +51,16 @@ const Navbar = () => {
           onClick={() => setOpenMenu(false)}
           onKeyDown={() => setOpenMenu(false)}
         >
-          <List>
-            {menuOptions.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
+<List>
+  {menuOptions.map((item) => (
+    <ListItem key={item.text} disablePadding>
+      <ListItemButton component="a" href={item.href}>
+        <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItemText primary={item.text} />
+      </ListItemButton>
+    </ListItem>
+  ))}
+</List>
           <Divider />
         </Box>
       </Drawer>
